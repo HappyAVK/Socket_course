9b3b80d37dea1eecfa500f5ff1b1426389c3e980
Alex Southall <119112983+HappyAVK@users.noreply.github.com>
2024-07-23 16:16:05 +0100

    Add files via upload

---
+++ threaded_client.py
@@ -0,0 +1,64 @@
+import socket, threading
+
+DEST_IP = socket.gethostbyname(socket.gethostname())
+DEST_PORT = 49153
+ENCODER = 'utf-8'
+BYTESIZE = 1024
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+client_socket.connect((DEST_IP, DEST_PORT))
+
+def send_message():
+    '''Send a message to a server to be broadcast'''
+    
+    while True:
+        message = input("message...")
+        client_socket.send(message.encode(ENCODER))
+
+
+def receive_message():
+    '''Receive a message from an incoming server'''
+    while True:
+        
+        try:
+            #Receive an incoming message from the server
+            message = client_socket.recv(BYTESIZE).decode(ENCODER)
+            
+            if message == "NAME":
+                
+                name = input("What is your name?:")
+                
+                client_socket.send(name.encode(ENCODER))   
+
+            else:
+                
+               # client_socket.send(message.encode(ENCODER))
+                print(message)
+        except:
+            #Close the connection due to error
+            
+            print("An error has occured, restart client")
+            client_socket.close()
+            break
+
+
+
+#Create to simultaeneously send and receive messages
+
+rec_thread = threading.Thread(target=receive_message)
+
+
+send_thread = threading.Thread(target=send_message)
+
+
+#Start the thread
+
+rec_thread.start()
+
+
+                
+send_thread.start()
+
+print("send a message")
---
+++ threaded_server.py
@@ -0,0 +1,111 @@
+import socket, threading
+
+HOST_IP = socket.gethostbyname(socket.gethostname())
+
+HOST_PORT = 49153
+
+ENCODER = 'utf-8'
+
+BYTESIZE = 1024
+
+#Create a server socket 
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind((HOST_IP, HOST_PORT))
+
+server_socket.listen()
+
+# a blank list of client_sockets 
+
+client_socket_list = []
+
+client_name_list = []
+
+def broadcast_message(message):
+    '''Send a message to all clients connected to the server'''
+    for client in client_socket_list:
+        msg = client.send(message.encode(ENCODER))
+
+pass
+
+def receive_message(client_socket):
+    '''Receive an incoming message from a specific client and forward the message to be broadcast'''
+    index = client_socket_list.index(client_socket)
+    name = client_name_list[index]
+    while True:
+        
+        try:
+            # get name of given client from list
+            
+            
+
+            message = client_socket.recv(BYTESIZE).decode(ENCODER)
+            print(client_socket)
+            message = f"\033[1;92m\t{name}: {message}\033[0m"
+
+            broadcast_message(message)
+
+        except:
+            
+
+            client_socket_list.remove(client_socket)
+
+            client_name_list.remove(name)
+
+            client_socket.close()
+
+            broadcast_message(f"\033[5;91m\t{name} has left the chat\033[0m")
+
+            break
+
+
+
+
+
+    
+
+def connect_client():
+    '''Connect an incoming client to the server'''
+
+    print('listening....')
+    while True:
+        # Accept any incoming client connection
+        
+
+        client_socket, client_address = server_socket.accept()
+
+        print(f"Connected with {client_address}...")
+
+        # sen d a name flag to prompt a client for their name
+
+        client_socket.send("NAME".encode(ENCODER))
+
+        client_name = client_socket.recv(BYTESIZE).decode(ENCODER)
+
+        #Add new client socket and client 
+
+        client_name_list.append(client_name)
+
+        client_socket_list.append(client_socket)
+
+
+        #update the server, induvidual client, and all clients
+
+        print(f"Name of new client is {client_name}\n")
+
+        client_socket.send(f"{client_name}, you have connected to the server!".encode(ENCODER))
+
+       # client_socket.send(f"{client_name}, you have joined\n".encode(ENCODER))
+
+        broadcast_message(f"{client_name} has joined!\n")
+
+        #Start a thread to receive messages after a connection
+
+        receive_thread = threading.Thread(target=receive_message, args=(client_socket,))
+
+        receive_thread.start()
+
+        
+
+connect_client()
